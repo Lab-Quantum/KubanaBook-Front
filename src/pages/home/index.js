aaef72a74a7d5ee805b6acee31c708e34acb9b78
Lucas Guidotti <dev.lucasguidotti@gmail.com>
2020-03-06 17:15:22 -0300

    Create NavBarTop

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './home.css';
+import NavBarTop from '../../components/navBarTop';
 
 const Home = () => {
     return( 
         <div className="home">
-            <div className="container"></div>
+            <div className="container">
+                <NavBarTop />
+            </div>
+            
         </div>
     );
 };
