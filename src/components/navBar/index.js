d743272732ebc0b9749370e45b8c35a7ca1edb05
Lucas Guidotti <dev.lucasguidotti@gmail.com>
2020-03-06 13:02:26 -0300

    Add logout button and modify snackbar position

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -9,32 +9,67 @@ import PerfilIcon from '../../assets/perfil.svg';
 import FeedIcon from '../../assets/feed.svg';
 import NotifyIcon from '../../assets/notify.svg';
 import AddIcon from '../../assets/add.svg';
+import LogoutIcon from '../../assets/logout.svg';
 
-const NavBar = () => (
-  <div className="navBar">
-    <div className="logoContainer">
-      <Link className="menuIten" to="/">
-        <img src={LogoFB} alt="logoFB" />
-      </Link>
-    </div>
-    <div className="menuContainer">
-      <Link className="menuIten" to="/">
-        <img src={HomeIcon} alt="HomeIcon" />
-      </Link>
-      <Link className="menuIten" to="/perfil">
-        <img src={PerfilIcon} alt="PerfilIcon" />
-      </Link>
-      <Link className="menuIten" to="/feed">
-        <img src={FeedIcon} alt="FeedIcon" />
-      </Link>
-      <Link className="menuIten" to="/notify">
-        <img src={NotifyIcon} alt="NotifyIcon" />
-      </Link>
-      <Link className="menuIten" to="/post">
-        <img src={AddIcon} alt="AddIcon" />
-      </Link>
-    </div>
-  </div>
+const NavBar = () => ( <
+    div className = "navBar" >
+    <
+    div className = "logoContainer" >
+    <
+    Link className = "menuIten"
+    to = "/" >
+    <
+    img src = { LogoFB }
+    alt = "logoFB" / >
+    <
+    /Link> < /
+    div > <
+    div className = "menuContainer" >
+    <
+    Link className = "menuIten"
+    to = "/" >
+    <
+    img src = { HomeIcon }
+    alt = "HomeIcon" / >
+    <
+    /Link> <
+    Link className = "menuIten"
+    to = "/perfil" >
+    <
+    img src = { PerfilIcon }
+    alt = "PerfilIcon" / >
+    <
+    /Link> <
+    Link className = "menuIten"
+    to = "/feed" >
+    <
+    img src = { FeedIcon }
+    alt = "FeedIcon" / >
+    <
+    /Link> <
+    Link className = "menuIten"
+    to = "/notify" >
+    <
+    img src = { NotifyIcon }
+    alt = "NotifyIcon" / >
+    <
+    /Link> <
+    Link className = "menuIten"
+    to = "/post" >
+    <
+    img src = { AddIcon }
+    alt = "AddIcon" / >
+    <
+    /Link> <
+    Link className = "menuIten"
+    to = "/logout" >
+    <
+    img src = { LogoutIcon }
+    alt = "Logout" / >
+    <
+    /Link> < /
+    div > <
+    /div>
 );
 
 export default NavBar;
