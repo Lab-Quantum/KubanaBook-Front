db83c97e88064a12c1f2d8f56b78545eb2f4f2c6
Cristian <contato@cristiancunha.me>
2020-03-12 10:35:29 -0300

    sandbox

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -8,10 +8,10 @@ import HomeIcon from '@material-ui/icons/HomeRounded';
 import PerfilIcon from '@material-ui/icons/PersonPinRounded';
 import FeedIcon from '@material-ui/icons/RssFeedRounded';
 import NotifyIcon from '@material-ui/icons/NotificationsRounded';
-import AddIcon from '@material-ui/icons/AddBoxRounded';
 import LogoutIcon from '@material-ui/icons/ExitToAppRounded';
 import Tooltip from "@material-ui/core/Tooltip";
 import Zoom from "@material-ui/core/Zoom";
+
 
 const NavBar = () => (
     <div className="navBar">
@@ -46,11 +46,11 @@ const NavBar = () => (
           </Link>
         </Tooltip>
 
-        <Tooltip title="Post" placement="right" TransitionComponent={Zoom}>
+        {/* <Tooltip title="Post" placement="right" TransitionComponent={Zoom}>
           <Link className="menuIten" to="/post">
             <AddIcon className="menuIcon" />
           </Link>
-        </Tooltip>  
+        </Tooltip>   */}
 
         <Tooltip title="Logout" placement="right" TransitionComponent={Zoom}>
           <Link className="menuIten" to="/logout">
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,13 +3,121 @@ import './home.css';
 import NavBarTop from '../../components/navBarTop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import IconButton from '@material-ui/core/IconButton';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
 const Home = () => {
+    const [open, setOpen] = React.useState(false);
+    const [openSnack, setOpenSnack] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setOpenSnack(true);
+    };
+
+    const handleCloseOnly = () => {
+        setOpen(false);
+    };
+
+    const handleCloseSnack = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenSnack(false);
+    };
+
+
     return( 
         <div className="home">
             <div className="container">
                 <NavBarTop />
                 <CircularProgress className="progressCircle"/>
+                <Fab onClick={handleClickOpen} style={{position: 'absolute', top: '90%', left: '95%'}} color="primary" aria-label="add">
+                    <AddIcon />
+                </Fab>
             </div>
+
+            {/* Modal */}
+            <Dialog 
+                open={open} 
+                onClose={handleClose} 
+                TransitionComponent={Transition} 
+                aria-labelledby="form-dialog-title" 
+                disableBackdropClick
+                disableEscapeKeyDown
+            >
+                <DialogTitle id="form-dialog-title">New Post</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Write a new post here. Send images, videos, text or even the same podcast.
+                        Ensure greater reach of your publication using the reach boost.
+                        <p>Att Quantum Lab</p>
+                    </DialogContentText>
+
+                    {/* IMG Input */}
+                    <input accept="image/*" style={{display: 'none'}} id="icon-button-file" type="file" />
+                        <label htmlFor="icon-button-file">
+                            <IconButton color="primary" aria-label="upload picture" component="span">
+                                <PhotoCamera />
+                            </IconButton>
+                        </label>
+
+                    {/* Text Input */}
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="New Post"
+                        type="text"
+                        fullWidth
+                    />
+                </DialogContent>
+
+                {/* Functional Buttons */}
+                <DialogActions>
+                    <Button onClick={handleCloseOnly} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Send
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* SnackBar */}
+            <Snackbar open={openSnack} autoHideDuration={6000} onClose={handleCloseSnack}>
+                <Alert style={{backgroundColor: '#3f51b5'}} onClose={handleCloseSnack} severity="success">
+                    This is a success message!
+                </Alert>
+            </Snackbar>
         </div>
     );
 };
