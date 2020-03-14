08991d70e8425fb8fa87f5189b7a2ca183d0e968
Cristian <contato@cristiancunha.me>
2020-03-14 17:56:15 -0300

    Change profile page icon to avatar component Material UI

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -12,6 +12,56 @@ import LogoutIcon from '@material-ui/icons/ExitToAppRounded';
 import Tooltip from "@material-ui/core/Tooltip";
 import Zoom from "@material-ui/core/Zoom";
 
+import Badge from '@material-ui/core/Badge';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+
+const StyledBadge = withStyles(theme => ({
+  badge: {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: '$ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}))(Badge);
+
+const SmallAvatar = withStyles(theme => ({
+  root: {
+    width: 22,
+    height: 22,
+    border: `2px solid ${theme.palette.background.paper}`,
+  },
+}))(Avatar);
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 
 const NavBar = () => (
   <div className="navBar">
@@ -28,12 +78,6 @@ const NavBar = () => (
         </Link>
       </Tooltip>
 
-      <Tooltip title="Profile" placement="right" TransitionComponent={Zoom}>
-        <Link className="menuIten" to="/profile">
-          <PerfilIcon className="menuIcon" />
-        </Link>
-      </Tooltip>
-
       <Tooltip title="Feed" placement="right" TransitionComponent={Zoom}>
         <Link className="menuIten" to="/feed">
           <FeedIcon className="menuIcon" />
@@ -46,18 +90,25 @@ const NavBar = () => (
         </Link>
       </Tooltip>
 
-      {/* <Tooltip title="Post" placement="right" TransitionComponent={Zoom}>
-          <Link className="menuIten" to="/post">
-            <AddIcon className="menuIcon" />
-          </Link>
-        </Tooltip>   */}
-
       <Tooltip title="Logout" placement="right" TransitionComponent={Zoom}>
         <Link className="menuIten" to="/logout">
           <LogoutIcon className="menuIcon" />
         </Link>
       </Tooltip>
 
+      <Link className="menuIten" to="/notify">
+        <StyledBadge style={{
+          width: '68px' ,
+          top: '93%',
+          position: 'fixed',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-end'
+          }}
+        >
+          <Avatar style={{width:'30px', height: '30px'}} alt="Cristian" />
+        </StyledBadge>
+      </Link>
     </div>
   </div>
 );
